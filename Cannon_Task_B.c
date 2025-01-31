e70125717a08a6bcbacc5d972c7af58a0a917ce2
Usama Sajjad <usamasajjad615@gmail.com>
2025-01-31 12:00:00 +0100

    Finalized scatter, gather, and checkerboard distribution for task B compliance

--- Cannon_Task_B.c
+++ Cannon_Task_B.c
@@ -157,3 +157,4 @@ int main(int argc, char** argv) {
 }// Commit for 2025-01-17 12:00:00 - Added print function for matrices
 // Commit for 2025-01-15 12:00:00 - Implemented matrix initialization and random value generation
 // Commit for 2025-01-19 12:00:00 - Developed Cannon’s algorithm structure with rank-based computation
+// Commit for 2025-01-31 12:00:00 - Finalized scatter, gather, and checkerboard distribution for task B compliance
